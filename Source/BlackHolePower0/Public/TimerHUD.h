6297e62cbed76b8dd3009bf9721ea8d4618cfa0f
maximecjr <conjardmaxime@gmail.com>
2024-11-08 15:59:51 -0500

    ajout de la classe HUD

---
+++ Source/BlackHolePower0/Public/TimerHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TimerHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKHOLEPOWER0_API ATimerHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};

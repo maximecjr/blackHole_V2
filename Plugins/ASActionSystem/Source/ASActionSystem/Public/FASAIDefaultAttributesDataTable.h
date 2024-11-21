a33ca55cd9b66674bba3e06d8adad81d35894be6
maximecjr <conjardmaxime@gmail.com>
2024-11-21 18:50:18 -0500

    datatable vide

---
+++ Plugins/ASActionSystem/Source/ASActionSystem/Public/FASAIDefaultAttributesDataTable.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "FASAIDefaultAttributesDataTable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASACTIONSYSTEM_API UFASAIDefaultAttributesDataTable : public UDataTable
+{
+	GENERATED_BODY()
+	
+};

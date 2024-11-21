26b7b07021ad50f34865423c6bfea5c0d492a6ba
Titouan Fréquelin <136023547+tfrequelt@users.noreply.github.com>
2024-11-21 18:23:19 -0500

    up

--- Plugins/ASActionSystem/Source/ASActionSystem/Public/ASActionSystemComponent.h
+++ Plugins/ASActionSystem/Source/ASActionSystem/Public/ASActionSystemComponent.h
@@ -6,7 +6,14 @@
 #include "Modules/ModuleManager.h"
 #include "Components/ActorComponent.h"
 #include "ASAttribute.h"
+#include "GameplayTagContainer.h"
 #include "ASActionSystemComponent.generated.h"
+
+
+DECLARE_DYNAMIC_DELEGATE_FourParams(FASAttributeChangeDelegate, FGameplayTag, AttributeTag, float, OldValue, float, NewValue, float, MaxValue);
+DECLARE_DYNAMIC_DELEGATE_FourParams(FASAttributeAddedDelegate, FGameplayTag, AttributeTag, float, InitialValue, float, MinValue, float, MaxValue);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FASAttributeRemovedDelegate, FGameplayTag, AttributeTag);
+
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ASACTIONSYSTEM_API UASActionSystemComponent : public UActorComponent
@@ -46,4 +53,56 @@ public:
 
     UFUNCTION(BlueprintCallable, Category = "Attributes")
     bool RemoveAttribute(FGameplayTag AttributeTag);
+
+    // Gestion des délégués pour les changements d'attributs
+    UFUNCTION(BlueprintCallable, Category = "Attributes")
+    int32 AddAttributeChangedDelegate(FGameplayTag AttributeTag, const FASAttributeChangeDelegate& Delegate);
+    UFUNCTION(BlueprintCallable, Category = "Attributes")
+    void RemoveAttributeChangedDelegate(int32 DelegateID);
+
+    // Gestion des délégués pour les ajouts d'attributs
+    UFUNCTION(BlueprintCallable, Category = "Attributes")
+    int32 AddAttributeAddedDelegate(const FASAttributeAddedDelegate& Delegate);
+    UFUNCTION(BlueprintCallable, Category = "Attributes")
+    void RemoveAttributeAddedDelegate(int32 DelegateID);
+
+    // Gestion des délégués pour les suppressions d'attributs
+    UFUNCTION(BlueprintCallable, Category = "Attributes")
+    int32 AddAttributeRemovedDelegate(const FASAttributeRemovedDelegate& Delegate);
+    UFUNCTION(BlueprintCallable, Category = "Attributes")
+    void RemoveAttributeRemovedDelegate(int32 DelegateID);
+
+protected:
+    TArray<FASAttributeChangedHolder> AttributeChangedDelegates;
+    TArray<FASAttributeAddedHolder> AttributeAddedDelegates;
+    TArray<FASAttributeRemovedHolder> AttributeRemovedDelegates;
+
+    int32 NextDelegateID = 0; // Générateur d'ID unique
+};
+
+USTRUCT()
+struct FASAttributeChangedHolder
+{
+    GENERATED_BODY()
+
+    int32 DelegateID;
+    FASAttributeChangeDelegate Delegate;
+};
+
+USTRUCT()
+struct FASAttributeAddedHolder
+{
+    GENERATED_BODY()
+
+    int32 DelegateID;
+    FASAttributeAddedDelegate Delegate;
+};
+
+USTRUCT()
+struct FASAttributeRemovedHolder
+{
+    GENERATED_BODY()
+
+    int32 DelegateID;
+    FASAttributeRemovedDelegate Delegate;
 };
